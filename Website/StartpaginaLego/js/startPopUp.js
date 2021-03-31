0493b16306147183c2a0a9166a91efd6fcd93d29
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-03-31 15:21:30 +0200

    main website

---
+++ Website/StartpaginaLego/js/startPopUp.js
@@ -0,0 +1,14 @@
+document.getElementById("button1").addEventListener("click",()=>{
+    document.querySelector(".popup").style.display = "flex";
+
+});
+
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".popup").style.display = "none";
+
+});
+
+document.querySelector(".button2").addEventListener("click",()=>{
+    document.querySelector(".popup").style.display = "none";
+
+});
