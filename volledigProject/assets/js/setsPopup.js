83cdbfee4638f6f20cab42d893b915f19cebb706
Ibragim Ismailov <ibragim.ismailov@student.ap.be>
2021-04-04 09:54:31 +0200

    lego sets pagina vervolledigt

---
+++ volledigProject/assets/js/setsPopup.js
@@ -0,0 +1,8 @@
+document.getElementById("button3").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("button4").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "none";
+
+});
