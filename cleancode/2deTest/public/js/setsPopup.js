4a6683a1d58da8c326c89934a0376937fc53e8c1
Hugues Kiendrebeogo <Hugues.kiendrebeogo@student.ap.be>
2021-05-27 14:17:47 +0200

    removed some unecessary files

--- cleancode/2deTest/public/js/setsPopup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("set_button").addEventListener("click",()=>{
-    document.querySelector(".set_popup").style.display = "flex";
-});
-document.querySelector(".close").addEventListener("click",()=>{
-    document.querySelector(".set_popup").style.display = "none";
-
-});
